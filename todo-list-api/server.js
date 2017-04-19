89d6154786449c7dbb1c9ffa8899f81113cfceb8
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-04-19 19:21:40 -0400

    updated server.js with the correct dependencies; added server instantiation code

--- todo-list-api/server.js
+++ todo-list-api/server.js
@@ -4,8 +4,18 @@
 
 	var express = require('express'),
 		app = express(),
-		port = process.env.PORT || 3000;
+		port = process.env.PORT || 3000,
+		mongoose = require('mongoose'),
+		Task = require('./api/models/todoListModel'),
+		bodyParser = require('body-parser'),
+		routes = require('./api/routes/todoListRoutes');
 
-	app.listen(port);
-	console.log('todo list RESTful API sever started on: ' + port);
+	mongoose.Promise = global.Promise;
+	mongoose.connect('mongodb://localhost/TodoDB');
+
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+
+	routes(app);
+	console.log('todo list RESTful API server started on port ' + port);
 }());
