1cebb46b6debceca1cff829bdf14daa777684ed5
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-08-13 09:25:48 -0400

    updated index.ts with a basic endpoint

--- node-hello-world/src/index.ts
+++ node-hello-world/src/index.ts
@@ -5,4 +5,17 @@ app.get('/', (request, response) => {
   response.send('hello world');
 });
 
+app.get('/api/sayhello/:name', (request, response) => {
+  let name = request.params.name;
+  if (!isNaN(name)) {
+    response.status(400)
+      .send('No string as a name');
+  } else {
+    response.json({
+      "message": name
+    });
+  }
+});
+
 app.listen(3000);
+console.log('listening on port 3000');
